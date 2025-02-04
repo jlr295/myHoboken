3ae35e270cf2058c1a09e469c5bd50feeb28e522
Jaclyn Rutter <jaclynrutter19@gmail.com>
2025-02-03 19:10:22 -0500

    Updated lists in DataSource

--- app/src/main/java/com/example/myHoboken/data/DataSource.kt
+++ app/src/main/java/com/example/myHoboken/data/DataSource.kt
@@ -1,4 +1,53 @@
 package com.example.myHoboken.data
 
+import com.example.myHoboken.R
+
 object DataSource {
+    val categories = listOf(
+        R.string.bars,
+        R.string.parks,
+        R.string.restaurants,
+        R.string.coffee_shops,
+        R.string.shopping
+    )
+
+    val bars = listOf(
+        R.string.belo_bar,
+        R.string.carpe_diem,
+        R.string.dear_maud,
+        R.string.pier_13,
+        R.string.string_ray
+    )
+
+    val parks = listOf(
+        R.string.church_square_park,
+        R.string.maxwell_park,
+        R.string.pier_a,
+        R.string.sinatra_park,
+        R.string.stevens_park
+    )
+
+    val restaurants = listOf(
+        R.string.augustinos,
+        R.string.karma_kafe,
+        R.string.morelias,
+        R.string.panela,
+        R.string.tosti
+    )
+
+    val coffee = listOf(
+        R.string.bwe,
+        R.string.mojo,
+        R.string.roost,
+        R.string.hive,
+        R.string.yukis
+    )
+
+    val shopping = listOf(
+        R.string.athleta,
+        R.string.sephora,
+        R.string.madewell,
+        R.string.lululemon,
+        R.string.thriftbox
+    )
 }
